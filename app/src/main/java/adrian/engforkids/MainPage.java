a765e1c8992b625d62dda72eb5a1071de8d60b92
AdrianKujawski <adrian.kujawski@oulook.com>
2016-10-26 10:06:12 +0200

    Dodany layout hie hie

--- app/src/main/java/adrian/engforkids/MainPage.java
+++ app/src/main/java/adrian/engforkids/MainPage.java
@@ -2,12 +2,24 @@ package adrian.engforkids;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainPage extends AppCompatActivity {
+
+    Button naukaButton;
+    Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        Initialization();
     }
+
+    private void Initialization(){
+        naukaButton = (Button) findViewById(R.id.NaukaButton);
+        testButton = (Button) findViewById(R.id.TestButton);
+    }
+
+
 }
